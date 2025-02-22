428369ed30ae0ce9ae63fb4f35a30262242116df
Kexin Sun <kexin.sun82@gmail.com>
2025-02-22 11:56:14 -0500

    Remove edit connection

--- pawpals/obj/Debug/net8.0/pawpals.AssemblyInfo.cs
+++ pawpals/obj/Debug/net8.0/pawpals.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pawpals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30a8c42b7b40006db04d620fd72899ebd3724c8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71dd66bcceaf1e2e69a12e26796b799c2a67996")]
 [assembly: System.Reflection.AssemblyProductAttribute("pawpals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pawpals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
